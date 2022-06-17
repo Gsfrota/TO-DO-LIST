0e9bfbcb4407c635f58361925193d0a43ef09fd9
Guilherme frota souza <80524785+Gsfrota@users.noreply.github.com>
2022-06-17 11:23:41 -0300

    v.0.2
    Resolvi alguns problemas como a barra de rolagem, porém ainda falta fazer a verificação das novas checkboxes

--- assets/index.js
+++ assets/index.js
@@ -2,29 +2,73 @@ let tarefas
 function inputTask(){
      tarefas = document.getElementById("tarefa").value;
     
+    
 }
 
 
+var count = 1;
+
+
+ 
 
 function evento(){
     //criar elemento
     /*tag br */
+   
+    
     let br = document.createElement("br");
 
     /*Cria a checkbox*/
     let newElementCheckbox = document.createElement("input");
-
-    newElementCheckbox.setAttribute("type", "checkbox");
-    newElementCheckbox.className = "checkbox";
+        newElementCheckbox.setAttribute("type", "checkbox");
+        newElementCheckbox.className = "checkbox";
     
-    let newLabel = document.createElement("label");
+        /*Cria o label*/ 
+   let newLabel = document.createElement("label");
+        /*Recebe a tarefa setada pelo input id= tarefa*/ 
+        newLabel.innerText= tarefas;
+        newLabel.className="label";
+    
+        /*Cira uma nova div*/
+    let newDiv = document.createElement("div");
+        newDiv.className = "checklist" 
+        newDiv.id = "checklistID" + count
+    
 
-    newLabel.innerText= tarefas;
-
-
+    /*Seleciona a section que irá conter as divs*/ 
     let element = document.getElementById("list");
-    element.appendChild(newElementCheckbox);
-    element.appendChild(newLabel);
-    element.appendChild(br);
+    
+    element.appendChild(newDiv);
+    
+    /*Seleciona a nova div criada*/ 
+     let element2 = document.querySelector("#checklistID"+count)
+    
+
+     element2.appendChild(newElementCheckbox);
+     element2.appendChild(newLabel);
+     element2.appendChild(br);
+     
+     count = count + 1;
+    
     
 }
+
+
+
+var checkbox = document.getElementsByClassName("checkbox");
+
+for (var i = 0; i < checkbox.length; i++) {
+    checkbox[i].addEventListener("change", checkedOrNot);
+  }
+  
+  function checkedOrNot() {
+    var isChecked = this.checked;
+  
+    if (isChecked) { //checked
+      console.log('checked');
+    } else { //unchecked
+      console.log('unchecked');
+    }
+  }
+ /*Verificação de checkbox para conclusão de tarefa */
+
